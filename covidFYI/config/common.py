f5ea242a9e3c739e65cb21b489f76b06bc3cd688
Tejas Arlimatti <tejasarlimatti@gmail.com>
2020-04-19 13:45:00 +0530

    Changed CORS whitelist to only point to our website

--- covidFYI/config/common.py
+++ covidFYI/config/common.py
@@ -48,13 +48,14 @@ class Common(Configuration):
     SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
     WSGI_APPLICATION = 'covidFYI.wsgi.application'
 
-    CORS_ORIGIN_ALLOW_ALL = True
+    CORS_ORIGIN_ALLOW_ALL = False
     CORS_ALLOW_CREDENTIALS = True
 
     CORS_ORIGIN_WHITELIST = [
-        'http://localhost:3000',
-        'http://localhost:8000',
-        'http://localhost:8080',
+        'https://covidfyi.in',
+        # 'http://localhost:3000',
+        # 'http://localhost:8000',
+        # 'http://localhost:8080',
     ]
 
     # Email
